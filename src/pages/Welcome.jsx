511758670da7a1541816cc284faaac09cb0411f4
margret <megajayi93@gmail.com>
2026-02-18 20:38:25 +0100

    welcome page styling done

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,64 +6,89 @@ const Welcome = () => {
   return (
     <>
       <nav className="welcomepageNav">
-        <div>MedStock AI</div>
         <div>
-          <button onClick={() => navigate("/signin")}>signin</button>
-          <button onClick={() => navigate("/login")}>Login</button>
+          <h3>MedStock AI</h3>
+        </div>
+        <div>
+          <button
+            onClick={() => navigate("/signin")}
+            className="welcomepageNavSignin"
+          >
+            sign up
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="welcomePageNavLogin"
+          >
+            Login
+          </button>
         </div>
       </nav>
       <div className="welcomePageHero">
-        <h1>Pharmacy Inventory Management</h1>
-        <p>
-          Streamline your pharmacy operations with AI- powered stock management,
-          demand forecasting, and real-time alerts.
+        <h1 className="welcomePageHeroH1">Pharmacy Inventory Management</h1>
+        <p className="welcomePageHeroP">
+          Streamline your pharmacy operations with AI- <br /> powered stock
+          management, demand forecasting,
+          <br /> and real-time alerts.
         </p>
-        <button onClick={() => navigate("/signin")}>Get Started</button>
+        <button
+          onClick={() => navigate("/signin")}
+          className="welcomePageHeroGetStarted"
+        >
+          Get Started
+        </button>
       </div>
-      <section>
-        <div>
+      <section className="smartSection">
+        <div className="smartSectionImage">
           <img src="../" alt="" />
         </div>
-        <h2>Smart Inventory Tracking</h2>
+        <h2 className="smartSectionH2">Smart Inventory Tracking</h2>
         <p>
           Track medicines with batch numbers, expiry dates, and real-time stock
           levels across multiple locations.
         </p>
       </section>
-      <section>
-        <div>
-          <div>
+      <section className="secondSection">
+        <div className="forcastDiv">
+          <div className="aiImageDiv">
             <img src="" alt="" />
           </div>
           <h3>AI Forecasts</h3>
           <p>Predict demand spikes</p>
         </div>
-        <div>
-          <div>
+        <div className="stocks">
+          <div className="aiImageDiv2">
             <img src="" alt="" />
           </div>
           <h3>Stock Alerts</h3>
           <p>Never run out of stock</p>
         </div>
       </section>
-      <search>
-        <h3>Pharmacy Health</h3>
-        <div>
+      <search className="pharmacy">
+        <h3 className="pharmacyH2">PHARMACY HEALTH</h3>
+        <div className="pharmacyScore">
           <h2>98.2%</h2>
-          <p>2.4%</p>
+          <div className="pharmacyP">
+            <p>2.4%</p>
+          </div>
         </div>
         <hr />
-        <p>Utilization</p>
+        <p className="util">UTILIZATION</p>
       </search>
-      <section>
+      <section className="createdWithAi">
         <div>
           <img src="" alt="" />
           <h3>Created with MedStock AI</h3>
         </div>
-        <button onClick={() => navigate("/signin")}>Get Started</button>
+        <button
+          onClick={() => navigate("/signin")}
+          className="createWithAiButton"
+        >
+          Get Started
+        </button>
       </section>
-      <footer>
-        <div>
+      <footer className="welcomePagefooter">
+        <div className="welcomePageDiv">
           <div>
             <h4>500+</h4>
             <p>Pharmacies</p>
