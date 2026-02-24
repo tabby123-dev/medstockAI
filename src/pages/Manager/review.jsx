6eef03a47244737a6440c61432296ec90f676477
margret <megajayi93@gmail.com>
2026-02-24 12:32:31 +0100

    review section almost completed

--- src/pages/Manager/review.jsx
+++ src/pages/Manager/review.jsx
@@ -29,22 +29,50 @@ const review = () => {
 
           <section>
             <div className="batchSection">
-              <div>
+              <div className="batchNumber">
                 <div>
-                  <h4>BATCH NUMBER</h4>
-                  <p>AMX-2024-0012</p>
+                  <h4 className="batchNumberH4">BATCH NUMBER</h4>
+                  <p className="batchNumberp">AMX-2024-0012</p>
                 </div>
-                <div>
-                  <h4>QUANTITY</h4>
-                  <p>+200 Units</p>
-                </div>
-              </div>
-              <div>
-                <h4>SUBMISSION HISTORY</h4>
-                <div>
-                  <p>Nurse Ugo Connor</p>
+                <div className="batchNumberDiv2">
+                  <h4 className="batchNumberH4">QUANTITY</h4>
+                  <p className="batchNumberp2">+200 Units</p>
                 </div>
               </div>
+              <div>
+                <h4 className="reviewExpiryDate">EXPIRY DATE</h4>
+                <div>
+                  <p className="reviewExpiryDatep">12 Oct 2026</p>
+                </div>
+              </div>
+              <div className="submissionhistory">
+                <h4 className="submissionhistoryh4">SUBMISSION HISTORY</h4>
+                <div>
+                  <p className="submissionhistoryp">Nurse Ugo Connor</p>
+                </div>
+                <div className="submissionistoryblackbox"></div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="reviewsectionID">
+              <div className="ID">
+                <h4>System Log ID:</h4>
+                <h4>Documentation:</h4>
+              </div>
+              <div className="ID2">
+                <p className="reviewsectionIDp">#LOG-88291-ZX</p>
+                <p className="reviewsectionIDp2">Invoice_AMX_012.pdf</p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="footersection">
+              <button className="footersectionBuutton">
+                Approve & Add to Stock
+              </button>
+              <button className="footersectionBuutton2">Reject Entry</button>
+              <div className="emtydiv"></div>
             </div>
           </section>
         </div>
